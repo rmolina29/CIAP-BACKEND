731a91de1c89cadf84cf8ec16d1b608601469d93
eschoonewolff <eschoonewolff@ises.com.co>
2023-12-21 17:38:11 -0500

    nuevos mensajes enum para utilizar mensajes personalizados

---
+++ src/mensajes_usuario/mensajes-usuario.enum.ts
@@ -0,0 +1,17 @@
+export enum MensajeAlerta {
+    ERROR = 'lo sentimos, algo salio mal',
+    UPS = 'Hemos encontrado un problema'
+}
+export enum RespuestaPeticion {
+    OK = 'ok'
+}
+
+export enum TipoEstado {
+    INACTIVO = 0,
+    ACTIVO = 1,
+    CONTRASENA_PRIMERA_VEZ = 0
+}
+export enum RegistroUsuario {
+    EXITOSO = 'Usuario registrado exitosamente',
+
+}
