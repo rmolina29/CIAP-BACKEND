72a7b1474bb95ec50f59cbec4b7494b0e71ea3f2
eschoonewolff <eschoonewolff@ises.com.co>
2023-12-25 20:50:39 -0500

    independizar modulo para envio de correos se pueda reutilizar

---
+++ src/crud_usuario/validaciones_crudUsuario/validaciones_usuario_crud.service.ts
@@ -0,0 +1,95 @@
+import { Injectable } from '@nestjs/common';
+import { CrudUsuarioService } from '../crud_usuario.service';
+import { CrudRolService } from '../crud_rol/crud_rol.service';
+import { DatosUsuario } from '../dtoCrudUsuario/crudUser.dto';
+import { RolEstado } from '../crud_rol/dto/rol.dto';
+
+@Injectable()
+export class ValidacionService {
+
+    constructor(private readonly sevicioUsuario: CrudUsuarioService, private readonly serivioRol: CrudRolService) { }
+
+    async validarIdRol(idRol: number): Promise<{ success: boolean, status?: number, message?: string }> {
+        const existeIdRol = await this.serivioRol.ExisteIdRol(idRol);
+        if (existeIdRol) {
+            return { success: false, status: 404, message: `El id del rol '${idRol}' no existe.` };
+        }
+        return { success: true };
+    }
+
+
+    async validarIdentificacion(identificacion: string, idUsuario: number): Promise<{ success: boolean, status?: number, message?: string }> {
+        const existeIdentificacion = await this.sevicioUsuario.existeIdentificacion(identificacion, idUsuario);
+        if (existeIdentificacion) {
+            return { success: false, status: 400, message: `La identificación '${identificacion}' ya se encuentra asociada a un usuario.` };
+        }
+        return { success: true };
+    }
+
+    async validarEmail(correo: string, idUsuario: number): Promise<{ success: boolean, status?: number, message?: string }> {
+        const existeEmail = await this.sevicioUsuario.existeEmail(correo, idUsuario);
+        if (existeEmail) {
+            return { success: false, status: 401, message: `El correo '${correo}' ya se encuentra asociado a un usuario.` };
+        }
+        return { success: true };
+    }
+
+    async validarProyecto(idProyecto: number[]): Promise<{ success: boolean, status?: number, message?: string }> {
+        const existeProyecto = await this.sevicioUsuario.existeProyecto(idProyecto);
+        if (!existeProyecto) {
+            return { success: false, status: 404, message: 'Por favor, verifique que los proyectos existan.' };
+        }
+        return { success: true };
+    }
+
+    async excepcionesRegistroUsuarios(usuario: DatosUsuario): Promise<{ success: boolean, status?: number, message?: string }> {
+
+        let idUsuario = usuario.idUsuario ?? 0;
+
+        const validacionIdRol = await this.validarIdRol(usuario.idRol);
+        const validacionIdentificacion = await this.validarIdentificacion(usuario.identificacion, idUsuario);
+        const validacionEmail = await this.validarEmail(usuario.correo, idUsuario);
+        const validacionProyecto = await this.validarProyecto(usuario.idProyecto);
+
+        if (!validacionIdRol.success) {
+            return validacionIdRol;
+        }
+
+        if (!validacionIdentificacion.success) {
+            return validacionIdentificacion;
+        }
+
+        if (!validacionEmail.success) {
+            return validacionEmail;
+        }
+
+        if (!validacionProyecto.success) {
+            return validacionProyecto;
+        }
+
+        return { success: true };
+    }
+
+
+    async rolActualizar(rol: RolEstado): Promise<{ success: boolean, status: number, message?: string, response: string }> {
+
+        const ExisteIdRol = await this.serivioRol.ExisteIdRol(rol.idRol);
+        const ValidacionRoLigado = await this.serivioRol.existeusuarioLigadoRol(rol.idRol);
+
+        if (ExisteIdRol) {
+            return { success: false, status: 404, message: `El id del rol '${rol.idRol}' no existe.`, response: 'no' };
+        }
+
+        if (ValidacionRoLigado) {
+            return { success: false, status: 401, message: `Este rol no se puede deshabilitar, debido a que se encuentra asociado a usuarios activos.`, response: 'no' };
+        }
+
+        return { success: true, status: 200, response: 'ok' };
+    }
+    
+ 
+}
+
+
+
+
---
+++ src/envio_correos/envio_correos.service.ts
@@ -0,0 +1,333 @@
+import { Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+import { Response } from 'express';
+import { datosObjetoCuerpoHtml } from 'src/auth/login/dto_autenticacion/usuario_autenticacion.dto'; 
+import { DatosToken } from 'src/restablecimiento_contrasena/restablecimiento_contra.dto/verificacion_correo.dto';
+import { Email } from './email.dto/email.dto';
+import { DatosUsuario } from 'src/crud_usuario/dtoCrudUsuario/crudUser.dto';
+
+
+@Injectable()
+export class EnvioCorreosService {
+    private transportador: nodemailer.Transporter;
+
+    constructor() {
+        this.transportador = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            port: process.env.MAIL_PORT,
+            secure: false,
+            auth: {
+                type: "login",
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            },
+        })
+    }
+    // colocar privado
+    async envio_correo(bodyString: string, correo: string) {
+
+        let res: Response;
+        // Lee el contenido HTML del archivo
+        try {
+
+            let body = {
+                from: process.env.MAIL_USER, // Remitente
+                to: correo, // Destinatario
+                subject: "Nuevo mensaje para el usuario", // Asunto del correo
+                html: `
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="utf-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <title>Template Mail CIAP</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+                        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css" />
+                </head>
+                
+                <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+                    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+                    crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+                    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+                    crossorigin="anonymous"></script>
+                
+                <style>
+                    @import url("https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100;0,9..40,200;0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;0,9..40,800;0,9..40,900;0,9..40,1000;1,9..40,100;1,9..40,200;1,9..40,300;1,9..40,400;1,9..40,500;1,9..40,600;1,9..40,700;1,9..40,800;1,9..40,900;1,9..40,1000&display=swap");
+                    .boderCus {
+                        margin-top: 150px;
+                        border: 6px solid #b3db7d8a !important;
+                        border-radius: 9px !important;
+                        padding-top: 8px;
+                        padding-right: 8px;
+                        padding-left: 8px;
+                    }
+                
+                    .TitleCus {
+                        font-family: "DM Sans", sans-serif;
+                        font-weight: 900;
+                        font-size: 17px;
+                        padding: 35px;
+                    }
+                
+                    .Saludo {
+                        font-family: "DM Sans", sans-serif;
+                    }
+                
+                    .nameUser {
+                        font-family: "DM Sans", sans-serif;
+                        font-size: 14px;
+                        font-weight: 900;
+                    }
+                
+                    .mensaje {
+                        font-family: "DM Sans", sans-serif;
+                        text-align: justify;
+                        font-size: 14px;
+                        padding-bottom: 15px;
+                    }
+                
+                    .btnCuston {
+                        width: 160px;
+                        font-family: "DM Sans", sans-serif;
+                        background: #b3db7d;
+                        display: block;
+                        margin: auto;
+                        border-radius: 24px;
+                        color: #b3db7d;
+                    }
+                
+                    .btnCuston:hover {
+                        background: #b3db7d;
+                        border: 1px solid #b3db7d;
+                    }
+                
+                    .btnSpaCuston {
+                        color: white;
+                        font-size: 15px;
+                        font-weight: bold;
+                        letter-spacing: 20px;
+                        margin-left: 20px;
+                    }
+                
+                    #contador {
+                        font-family: "DM Sans", sans-serif;
+                        font-size: 14px;
+                    }
+                
+                    .note {
+                        padding-top: 22px;
+                        font-family: "DM Sans", sans-serif;
+                        font-size: 12px;
+                        font-style: italic;
+                        font-weight: bold;
+                        text-align: justify;
+                        margin-bottom: -10px;
+                    }
+                
+                    .toast {
+                        background: white;
+                        position: absolute;
+                        margin-left: 93px;
+                        margin-top: -140px;
+                        z-index: 999 !important;
+                
+                    }
+                
+                    #txtCopy {
+                        font-family: "DM Sans", sans-serif;
+                        font-weight: bold;
+                    }
+                
+                    .headerCus {
+                        display: block;
+                        margin: auto;
+                        background: #d6ecb9;
+                        width: 105px;
+                        height: 50px;
+                        font-size: 25px;
+                        text-align: center;
+                        color: white;
+                        border-radius: 8px;
+                        margin-top: -50px;
+                        z-index: -1;
+                    }
+                </style>
+                
+                
+                <body>
+                   ${bodyString}
+                </body>
+                
+                </html>`,
+
+            };
+
+            this.transportador.sendMail(body);
+
+        } catch (error) {
+            console.error('Error al enviar correo:', error);
+            res.status(500).json({ mensaje: 'Hemos tenido un problema al enviar el correo' });
+        }
+
+    }
+
+    // Este cuerpo es dedicado para el envio de la informacion de la cuenta bloqueada
+    cuerpoHtmlCuentaBloqueada(datosUsuario: any,datosObjetoCuerpoHtml:datosObjetoCuerpoHtml): string {
+        return `
+        <div class="container">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col"></div>
+                                <div class="col-10">
+                                    <div class="card boderCus">
+                                        <div class="card-body">
+                                            <h5 class="card-title TitleCus text-center">
+                                                Bloqueo de cuenta
+                                            </h5>
+                                            <p class="card-text Saludo">
+                                                <span id="Saludo">Buen día,</span>
+                                                <span id="user" class="nameUser">${datosUsuario.nombres} ${datosUsuario.apellidos}</span>
+                                            </p>
+                                            <br>
+                                            <br>
+                                            <p class="card-text mensaje">
+                                             Se le informa que se ha bloqueado su cuenta, despues de tres (${datosObjetoCuerpoHtml.cantidadLoginValidos}) intentos fallidos de acceso incorrecto.
+                                             ${datosObjetoCuerpoHtml.tiempoRelogin} minutos a partir de la recepcion de este correo.
+                                            </p>
+
+                                            <br>
+
+                                            <p class="card-text mensaje">
+                                                la cuenta se desbloqueará el <strong>${datosObjetoCuerpoHtml.fechaBloqueo}</strong>
+                                            </p>
+                
+                                            <p class="note">
+                                                Este mensaje de correo se le ha enviado de forma automática.
+                                                Por favor, no intente enviar correos a la dirección de este
+                                                mensaje.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                             
+                            </div>
+                        </div>
+                    </div>
+                </div>
+     `
+    }
+
+
+    CuerpoTokenUsuario(datosToken: DatosToken, usuario_valido: Email) {
+        return `
+        <div class="container">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col"></div>
+                                <div class="col-10">
+                                    <div class="card boderCus">
+                                        <div class="card-body">
+                                            <h5 class="card-title TitleCus text-center">
+                                                Restablecimiento de contraseña
+                                            </h5>
+                                            <p class="card-text Saludo">
+                                                <span id="Saludo">Buen día,</span>
+                                                <span id="user" class="nameUser">${usuario_valido.nombres} ${usuario_valido.apellidos}</span>
+                                            </p>
+                                            <p class="card-text mensaje">
+                                                Para continuar con el proceso de recuperación de contraseña,
+                                                ingrese el siguiente código de verificación:
+                                            </p>
+                                            <button type="button" class="btn btnCuston" id="toastbtn" data-bs-toggle="tooltip"
+                                                data-bs-placement="right" data-bs-title="Toca para copia tu codigo">
+                                                <span class="btnSpaCuston">${datosToken.clave}</span>
+                                            </button>
+                
+                                            <br>
+                                            <p id="contador">El código expira el <b>${datosToken.fechaExpiracion}</b></p>
+                
+                                            <p class="note">
+                                                Este mensaje de correo se le ha enviado de forma automática.
+                                                Por favor, no intente enviar correos a la dirección de este
+                                                mensaje.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                             
+                            </div>
+                        </div>
+                    </div>
+
+                    </div>
+     `
+    }
+
+    CuerpoRegistroUsuario(nombreUsuario: string, contrasena: string, datosUsuario: DatosUsuario) {
+        return `
+        <div class="container">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col"></div>
+                                <div class="col-10">
+                                    <div class="card boderCus">
+                                        <div class="card-body">
+                                            <h5 class="card-title TitleCus text-center">
+                                            Credenciales de acceso - CIAP
+                                            </h5>
+                                            <p class="card-text Saludo">
+                                                <span id="Saludo">Buen día,</span>
+                                                <span id="user" class="nameUser">${datosUsuario.nombres} ${datosUsuario.apellidos}</span>
+                                                <span>Reciba un cordial saludo.</span>
+                                            </p>
+                                            <p class="card-text mensaje">
+                                                A continuación, le enviamos sus credenciales de acceso a la plataforma CIAP:
+                                            </p>
+
+                                        
+                                            <button type="button" class="btn btnCuston" id="toastbtn" data-bs-toggle="tooltip"
+                                                data-bs-placement="right" data-bs-title="Toca para copia tu codigo">
+                                                <span class="btnSpaCuston">Usuario: ${nombreUsuario}</span>
+                                            </button>
+
+                                            <button type="button" class="btn btnCuston" id="toastbtn" data-bs-toggle="tooltip"
+                                                data-bs-placement="right" data-bs-title="Toca para copia tu codigo">
+                                                <span class="btnSpaCuston">Contraseña: ${contrasena}</span>
+                                            </button>
+                
+                                            <br>
+
+                                            <p class="">
+                                            Al ingresar al sistema, le solicitaremos el cambio de contraseña. 
+                                            Debe tener en cuenta las políticas de seguridad que se le indicarán.
+                                            </p>
+
+                                            <br>
+                                            <br>
+
+                                            <p class="">
+                                            Para acceder a la plataforma CIAP, haga clic <button type="button" class="btn btnCuston" id="toastbtn" data-bs-toggle="tooltip"
+                                            href="https://chat.openai.com/c/5d2dcf31-8c31-42bc-bb52-3560974570fb"  data-bs-placement="right" data-bs-title="Toca para copia tu codigo">
+                                            <span class="btnSpaCuston">click</span>
+                                        </button>
+                                            </p>
+                
+                                            <p class="note">
+                                                Este mensaje de correo se le ha enviado de forma automática.
+                                                Por favor, no intente enviar correos a la dirección de este
+                                                mensaje.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                             
+                            </div>
+                        </div>
+                    </div>
+
+                    </div>
+     `
+    }
+}
